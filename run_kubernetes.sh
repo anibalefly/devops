c2dda8a3aba954f661aac09fb855dd190db051a6
Jeffrey Tan <tannedlamb@gmail.com>
2020-08-19 15:37:54 +0100

    Udacity project updates

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,17 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=tannedlamb/udacity_devops
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run flask --image=tannedlamb/udacity_devops
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward deployment/flask 8000:80
 
